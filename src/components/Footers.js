6b2e30060459762ba075fe448d1a40059709f724
bogdan-cretu <cretu.bogdan@yahoo.ro>
2025-02-17 20:43:21 +0200

    minor updates

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -12,7 +12,7 @@ function Footers() {
         <h3 className='title'>Link-uri rapide:</h3>
         <a className='margin-bottom' href=''>Politica de confidențialitate</a>
         <a href=''>Termeni și condiții</a>
-        <p>Copyright: „© <span id="year"></span> Thai Ice Cream Rolls. Toate drepturile rezervate.”</p>
+        <p>Copyright: „© <span id="year"></span> Thai Ice Cream Rolls. <br></br>Toate drepturile rezervate.”</p>
     </div>
   )
 }
